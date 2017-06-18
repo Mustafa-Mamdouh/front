75ceefbfc481d45005d72f37a331b7ba692c78b1
Mustafa <MustafaMamdouh@outlook.com>
2017-06-18 14:23:18 +0200

    LookupTables

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -14,15 +14,21 @@ import { ViewOrder } from './Order/viewOrder.component'
 import { KeysPipe } from './Order/key.pipe'
 import { ComponentService } from './Component/component.service';
 import { ComponentComponent } from './Component/component.component';
+import { LookupService } from './LookupTables/Lookup.service';
+import { DeliveryTypeComponent } from './LookupTables/DeliveryType.component';
+import { ServiceCategoryComponent } from './LookupTables/ServiceCategory.Component';
+import { EmployeeDiscountComponent } from './LookupTables/EmployeeDiscount.Component';
+
 import{ServiceComponent} from'./Service/service.component'
 import { HomeComponent } from './home/home.component';
 import { HomeService } from './home/home.service';
 import{ServiceService} from'./Service/service.service'
 @NgModule({
-  providers: [CategoryService, OrderService, ComponentService, HomeService,ServiceService],
+  providers: [CategoryService, OrderService, ComponentService, HomeService,ServiceService,LookupService],
   imports: [BrowserModule, FormsModule, HttpModule, AppRoutingModule],
   declarations: [AppComponent, CategoryComponent, AddCategory, EditCategoryComponent,
-    AddOrderComponent, ViewOrder, KeysPipe, ComponentComponent, HomeComponent,ServiceComponent],
+    AddOrderComponent, ViewOrder, KeysPipe, ComponentComponent, HomeComponent,ServiceComponent,DeliveryTypeComponent
+    ,ServiceCategoryComponent,EmployeeDiscountComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- front/src/app/navigation/app.nav.ts
+++ front/src/app/navigation/app.nav.ts
@@ -6,8 +6,10 @@ import { EditCategoryComponent } from '../category/editCategory.component'
 import { AddOrderComponent } from '../Order/addOrder.component'
 import { ViewOrder } from '../Order/viewOrder.component';
 import { ComponentComponent } from '../Component/component.component'
-
-import {ServiceComponent} from'../Service/service.component'
+import { DeliveryTypeComponent } from '../LookupTables/DeliveryType.component'
+import { ServiceCategoryComponent } from '../LookupTables/ServiceCategory.Component';
+import { EmployeeDiscountComponent } from '../LookupTables/EmployeeDiscount.Component';
+import { ServiceComponent } from '../Service/service.component'
 import { HomeComponent } from '../home/home.component'
 
 const APP_ROUTE: Routes = [
@@ -18,7 +20,12 @@ const APP_ROUTE: Routes = [
     { path: 'order/viewAll', component: ViewOrder },
     { path: 'component/:id', component: ComponentComponent },
     { path: 'category', component: CategoryComponent },
-    { path: 'service', component: ServiceComponent }
+    { path: 'service', component: ServiceComponent },
+    { path: 'deliveryType', component: DeliveryTypeComponent },
+    { path: 'ServiceCategory', component: ServiceCategoryComponent },
+    { path: 'EmployeeDiscount', component: EmployeeDiscountComponent }
+
+
 ];
 @NgModule({
     imports: [
